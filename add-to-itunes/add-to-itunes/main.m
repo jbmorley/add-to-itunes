6c9d8e6ee89b30a5d4f8e4fa2e00e9598b47ab08
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-11-29 19:17:42 +0100

    Initial commit of a shell application and Podfile.

---
+++ add-to-itunes/add-to-itunes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  add-to-itunes
+//
+//  Created by Jason Barrie Morley on 29/11/2014.
+//  Copyright (c) 2014 InSeven Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
