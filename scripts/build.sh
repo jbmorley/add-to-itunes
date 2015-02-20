f0f3e6bf500367ac55be0a0b0bb7383dd9651956
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-02-20 05:58:52 +0000

    Ignoring the build directory and updating the script to build intermediates in-tree.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,25 @@
 set -e
 set -u
 
-pushd add-to-itunes
+function normalize() {
+    echo $( cd "$1" && pwd )
+}
+
+scripts_directory=$(normalize $(dirname "${BASH_SOURCE[0]}"))
+root_directory=$(normalize "$scripts_directory/..")
+source_directory="$root_directory/add-to-itunes"
+build_directory="$root_directory/build"
+
+if [[ -e "$build_directory" ]]; then
+    echo "Removing '$build_directory'..."
+    rm -r "$build_directory"
+fi
+
+echo "Creating '$build_directory'..."
+mkdir -p "$build_directory"
+
+pushd "$source_directory" > /dev/null
 echo "Updating CocoaPods..."
 pod install --silent
-xcodebuild -workspace add-to-itunes.xcworkspace -scheme add-to-itunes clean build | xcpretty -c
-popd
+xcodebuild -workspace add-to-itunes.xcworkspace -scheme add-to-itunes -derivedDataPath "$build_directory" clean build | xcpretty -c
+popd > /dev/null
